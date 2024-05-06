9550c0cc9b21d104fd45cbc41e28e51b2ec3acbf
aymanreda56 <58632281+aymanreda56@users.noreply.github.com>
2024-05-06 18:29:05 +0300

    tany

--- auto_update.py
+++ auto_update.py
@@ -4,23 +4,6 @@ import datetime
 import dateutil.parser
 import wget
 import subprocess
-import shutil, os
-
-
-def move_files_inside_folder_to_outside(folder_path):
-    # Get a list of all files inside the folder
-    files = os.listdir(folder_path)
-    
-    # Move each file to the parent directory
-    for file_name in files:
-        # Construct the source and destination paths
-        source = os.path.join(folder_path, file_name)
-        destination = os.path.join(os.path.dirname(folder_path), file_name)
-        
-        # Move the file
-        shutil.move(source, destination)
-
-
 
 r = requests.get("https://api.github.com/repos/aymanreda56/test/commits")
 
@@ -38,10 +21,7 @@ latest_version = dateutil.parser.parse(entry_date)
 if(current_version < latest_version):
     # url = 'http://github.com/aymanreda56/test/archive/main.zip'
     # filename = wget.download(url)
-    subprocess.run('git clone https://github.com/aymanreda56/test')
-    folder_path = "test"
-    move_files_inside_folder_to_outside(folder_path)
-    shutil.rmtree('test')
+    subprocess.run('git pull https://github.com/aymanreda56/test')
 
 
 

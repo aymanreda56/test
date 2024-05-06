6d0e3c05d1a559db9bce56478e2d61ca46b053af
aymanreda56 <58632281+aymanreda56@users.noreply.github.com>
2024-05-06 17:27:40 +0300

    ww

--- auto_update.ps1
+++ auto_update.ps1
@@ -1,2 +1,5 @@
 #then do:
-curl https://api.github.com/repos/aymanreda56/test/commits/master
+curl https://api.github.com/repos/aymanreda56/test/commits/main -H @{"Accept"= "application/json"} 2>&1 | sls "date"
+# $RESPONSE = curl -v https://api.github.com/repos/aymanreda56/test/commits/main 2>&1 | grep '"date"'
+
+# Write-Host $RESPONSE
